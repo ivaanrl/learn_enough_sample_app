becd7ad7e55e7caa26dcb692d4895e1a2088ba0f
ivaanrl <ivanrl2010@gmail.com>
2019-10-03 09:44:47 -0300

    Implement advanced login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
